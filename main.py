0a0067913454c56391d557c88473a5707640514f
Jana Wilbert <jana.wilbert@posteo.de>
2023-06-21 16:53:28 +0200

    implement correct guesses

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 import data
 import random
 
-# create words to guess
+"""
+create words to guess
+"""
 words = ['FRIDGE', 'PYTHON', 'RIVER', 'FlOWER', 'NEIGHBOOR']
 word = random.choice(words)
 
-# document wrong guesses in a list
+"""
+create list to document wrong guesses in a list
+"""
 wrong_guesses = []
 
-# display partial solution of the word
+"""
+create display of partial solution of the word
+"""
 partial_solution = len(word) * "_"
 
-# print gallow (from data.py) and partial solution
+"""
+print gallow (from data.py) and partial solution
+"""
 print(data.gallows[len(wrong_guesses)]) # gallow with the index of the number of wrong guesses
 print(f"Your word: {partial_solution}")
 
+"""
+while there are still guesses left, ask for input and give feedback
+"""
 while len(wrong_guesses) < len(data.gallows) - 1:
-    letter = input("Your guess: ").upper()
+    letter = input("Your guess: ").upper() # get letter as an input
     if letter in word:
-        pass #TODO: handle correctly guessed characters
+        for i, x in enumerate(word):
+            if x == letter:
+                partial_solution = partial_solution[:i] + letter + partial_solution[i+1:]
     else:
-        if letter not in wrong_guesses:
+        if letter not in wrong_guesses: # check whether wrong letter was guessed before and add to wrong guesses list
             wrong_guesses.append(letter)
     print(data.gallows[len(wrong_guesses)])
     print(f"Your word: {partial_solution}, Wrong guesses: {', '.join(wrong_guesses)}")
