eacdd458c9641bf783a5c9dd6bd1a35a625e83c5
Jana Wilbert <jana.wilbert@posteo.de>
2023-06-21 17:30:13 +0200

    prettifying

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 """
 create words to guess
 """
-words = ['FRIDGE', 'PYTHON', 'RIVER', 'FlOWER', 'NEIGHBOOR']
+words = ['FRIDGE', 'PYTHON', 'RIVER', 'FLOWER', 'NEIGHBOOR']
 word = random.choice(words)
 
 """
@@ -21,7 +21,7 @@ partial_solution = len(word) * "_"
 print gallow (from data.py) and partial solution
 """
 print(data.gallows[len(wrong_guesses)]) # gallow with the index of the number of wrong guesses
-print(f"Your word: {partial_solution}")
+print(f"🎯 Your word: {partial_solution}")
 
 """
 while guesses left & word not fully guessed yet, ask for input and give feedback
@@ -36,12 +36,14 @@ while len(wrong_guesses) < len(data.gallows) - 1 and partial_solution != word:
         if letter not in wrong_guesses: # check whether wrong letter was guessed before and add to wrong guesses list
             wrong_guesses.append(letter)
     print(data.gallows[len(wrong_guesses)])
-    print(f"Your word: {partial_solution}, Wrong guesses: {', '.join(wrong_guesses)}")
+    print(f"🎯 Your word: {partial_solution}")
+    print(f"❌ Wrong guesses: {', '.join(wrong_guesses)}")
+    print(f"✅ Number of guesses still left: {len(data.gallows) - len(wrong_guesses) - 1}\n")
 
 """
 give feedback whether the game was won or lost
 """
 if word == partial_solution:
-    print("Congrats, you have won!")
+    print("Congrats, you have won!\n")
 else:
-    print(f"Oh no, you have lost! The word was: {word}")
+    print(f"Oh no, you have lost! The word was: {word}\n")
