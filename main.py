6625aded36e2f0112bd739a0ef3534ae95e01351
Jana Wilbert <jana.wilbert@posteo.de>
2023-06-21 15:58:39 +0200

    add wrong guesses response

--- main.py
+++ main.py
@@ -14,3 +14,13 @@ partial_solution = len(word) * "_"
 # print gallow (from data.py) and partial solution
 print(data.gallows[len(wrong_guesses)]) # gallow with the index of the number of wrong guesses
 print(f"Your word: {partial_solution}")
+
+while len(wrong_guesses) < len(data.gallows) - 1:
+    letter = input("Your guess: ").upper()
+    if letter in word:
+        pass #TODO: handle correctly guessed characters
+    else:
+        if letter not in wrong_guesses:
+            wrong_guesses.append(letter)
+    print(data.gallows[len(wrong_guesses)])
+    print(f"Your word: {partial_solution}, Wrong guesses: {', '.join(wrong_guesses)}")
